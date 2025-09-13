d7635247bef07ddb491d4f4cb19a17cc9ac33aa1
divneev15 <divneevsingh123@gmail.com>
2025-09-13 10:41:40 +0530

    Create Constructor_overloading_2.cpp

---
+++ Constructor_overloading_2.cpp
@@ -0,0 +1,40 @@
+/* Divneev Singh
+PRN 24070123043
+ENTC A2 */
+
+#include<iostream>
+using namespace std;
+
+class Volume
+{
+    public:
+  Volume(float l, float b, float h)
+  {
+    float vol;
+    vol = l*b*h;
+    cout<<"\nthe volume of the cuboid is : "<<vol<<endl;
+  }  
+
+  Volume(float s)
+  {
+    float vol1;
+    vol1 = s*s*s;
+    cout<<"\nThe volume of cube is : "<<vol1<<endl;
+  }
+
+}; // end of class
+
+int main()
+{
+    Volume v1(2.5f, 1.5f, 5.2f);
+    Volume v2(5);
+
+}
+
+/* output for this code
+
+the volume of the cuboid is : 19.5
+
+The volume of cube is : 125
+
+  */
